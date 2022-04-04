cbf7bf3144e62e8bbcad78cb784f9000ed1da5c7
Philipp Strauß <phil13.strauss@gmail.com>
2022-04-04 19:18:42 +0200

    finished text to morse translation

--- morse.js
+++ morse.js
@@ -1,10 +1,10 @@
 
 // create way to accept input
-let message = 'sos, sos';
+let message = 'if only i knew what i was doing. heck, heck oh boy.';
 
 // define translator list of character-dot/dash
-const greek = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', ' ', '/'];
-const dotdash = ['.-', '-...', '-.-.', '-..', '.', '..-.', '--.', '....', '..', '.---', '-.-', '.-..', '--', '-.', '---', '.--.', '--.-', '.-.', '...', '-', '..-', '...-', '.--', '-..-', '-.--', '--..', '/', ' '];
+const greek = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', ' ', '.'];
+const dotdash = [' .-', ' -...', ' -.-.', ' -..', ' .', ' ..-.', ' --.', ' ....', ' ..', ' .---', ' -.-', ' .-..', ' --', ' -.', ' ---', ' .--.', ' --.-', ' .-.', ' ...', ' -', ' ..-', ' ...-', ' .--', ' -..-', ' -.--', ' --..', ' ', ' //'];
 const translated = [];
 
 
@@ -14,17 +14,13 @@ for (i = 0; i <= message.length; i++){
        // console.log(greek.indexOf(message[i]))
         translated.push(dotdash[greek.indexOf(message[i])])
     }
-    if (dotdash.includes(message[i])){
-       // console.log(dotdash.indexOf(message[i]))
-        translated.push(greek[dotdash.indexOf(message[i])])
-    }
+    // if (dotdash.includes(message[i])){
+    //    // console.log(dotdash.indexOf(message[i]))
+    //     translated.push(greek[dotdash.indexOf(message[i])])
+    // }
 };
-console.log(translated);
-// console.log(translated);
-
-
-
-// for array greek[index] instead return dotdash[index]
-// for array dotdash[index] instead return greek[index]
-
-// give translated output
+console.log(translated.join(''));
+// find a way to revere translate, avoid ambiguity
+let morseMessage = '... --- ...';
+const isolatedDotDash = morseMessage.split(' ');
+console.log(isolatedDotDash);
